6a52806737f49251ed46081be1cf3197f6c48027
Lucca Antonio Menegatti <menegatti.luccaantonio@gmail.com>
2022-12-21 02:59:08 -0300

    Create Class Position

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace xadrez_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("posicao: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
